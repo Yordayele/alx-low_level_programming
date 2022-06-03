9f01c7873665d85562cf8d467c867eda9195f772
Yordayele <yordanosayele8@gmail.com>
2022-06-02 18:22:15 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
-
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - Entry point
+ *   
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	    return (0);
-
+printf("size of a char: %d byte(a)\n", sizeof(char));
+printf("size of an int: %d byte(a)\n", sizeof(int));
+printf("size of long int: %d byte(a)\n", sizeof(long int ));
+printf("size of long long: %d byte(a)\n", sizeof(long long));
+printf("size of float: %d  byte(a)\n", sizeof(float));
+return (0);
 }
