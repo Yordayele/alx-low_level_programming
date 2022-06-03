7ac085f225a67fda508323f9d738cb5cb7b80cf4
Yordayele <yordanosayele8@gmail.com>
2022-06-02 17:36:53 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+     print("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+    return (1);
+}
