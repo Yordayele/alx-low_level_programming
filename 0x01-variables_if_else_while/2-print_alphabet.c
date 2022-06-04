c83cd9fcccb93c6dc35b4998fb63a61cd6161ab0
Yordayele <yordanosayele8@gmail.com>
2022-06-04 07:10:33 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**                                                               
+ * Description: main - to check if the number is lower case                  
+ *
+ * Return: returns 0 if everything is successful                                 *
+ **/   
+int main(void)
+{
+	for (int x = 97; x = 122; ++x)
+		putchar("%c\n ", x);
+	return (0);
+}
