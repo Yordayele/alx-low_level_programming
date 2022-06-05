db8e948e79856cac86201fb981539d1faae5b56d
Yordayele <yordanosayele8@gmail.com>
2022-06-05 04:00:17 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /*
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.
+ * Description: main - prints "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19", followed by a new line, to standard error.
  *
  * Return: Always 1.
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(59);
 	return (1);
 }
