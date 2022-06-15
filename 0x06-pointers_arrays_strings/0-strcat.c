ec0129d5569d708d155be2fe3f6fb1a26ffb8669
Yordayele <yordanosayele8@gmail.com>
2022-06-15 16:57:47 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,18 @@
  */
 char *strcat(char *dest, const char *src)
 {
-int index = 0, dest_len = 0;
-
-while (dest[index++])
-dest_len++;
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
+int i, j;
+i = 0;
+j = 0;
+while (dest[i] != '\0')
+{
+i++
 }
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
