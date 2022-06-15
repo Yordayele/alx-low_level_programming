6bd64ad32826463323bbfed64b14b40527d9460b
Yordayele <yordanosayele8@gmail.com>
2022-06-15 15:20:22 -0700

    task 0

---
+++ 0-strcat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * strcat -  Concatenates the string pointed to by @src, including the
+ * terminating null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src:The source string to be appended to @dest.
+ */
+char *strcat(char *dest, const char *src)
+{
+int index = 0, dest_length = 0;
+
+while (dest[index++])
+dest_length++;
+for (index = 0; src[index]; index++)
+dest[dest_length++] = src[index];
+return (dest);
+}
