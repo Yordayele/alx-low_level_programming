9634afa4f083d618e182a2f784497a22db7de606
Yordayele <yordanosayele8@gmail.com>
2022-06-15 19:12:46 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,18 @@
 #include <stdio.h>
-/** main - cause an ininite loop
- *
+/** main - causes an ininite loop.
  * return: 0.
  */
 int main(void)
 {
-	int i;
+int i;
 
-	printf("displaying infinite loop:\n");
-		i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-	}
-
-	printf("infinite loop avoided\n");return (0);
+printf("displaying infinite loop:\n");
+i = 0;
+while (i < 10)
+{
+putchar(i);
+}
+printf("infinite loop avoided\n");
+return (0);
 }
 
